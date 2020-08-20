29865ba11eb2f2ff09ece64a50989b99223b2f7e
Harvey Rankin <harveyrankin@live.co.uk>
2020-08-20 09:48:59 +0100

    added icon buttons

--- front_end/my-app/src/components/header/Header.js
+++ front_end/my-app/src/components/header/Header.js
@@ -2,13 +2,20 @@ import React from "react";
 import PersonIcon from '@material-ui/icons/Person';
 import QuestionAnswerIcon from '@material-ui/icons/QuestionAnswer';
 import "./Header.css";
+import IconButton from "@material-ui/core/IconButton";
 
 const Header = () => {
     return(
     <div className="header">
-        <PersonIcon/>
-        <img className="header__logo" src="https://www.logosurfer.com/wp-content/uploads/2018/03/tinder-logo_0.png" alt="tinder logo"/>
-        <QuestionAnswerIcon/>
+        <IconButton>
+        <PersonIcon className="header__icon" fontSize="large"/>
+        </IconButton>
+
+        <img style={{height:'40px',objectFit:'contain'}} src="https://www.logosurfer.com/wp-content/uploads/2018/03/tinder-logo_0.png" alt="tinder logo"/>
+        <IconButton>
+        <QuestionAnswerIcon className="header__icon" fontSize="large"/>
+        </IconButton>
+        
     </div>
     )
 }
