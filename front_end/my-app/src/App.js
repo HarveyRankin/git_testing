397332cccd3b01f7fd7f5b5bc23d000b69829331
Harvey Rankin <harveyrankin@live.co.uk>
2020-08-20 13:44:08 +0100

    changing

--- front_end/my-app/src/App.js
+++ front_end/my-app/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Header from "./components/header/Header";
-import {BrowserRouter as Router,Switch,Route,Link} from "react-router-dom"; 
+import {BrowserRouter as Router,Switch,Route} from "react-router-dom"; 
 import TinderCards from './components/cards/Cards';
 import SwipeButtons from './components/swipeButtons/SwipeButtons';
 
@@ -10,14 +10,13 @@ import SwipeButtons from './components/swipeButtons/SwipeButtons';
 function App() {
   return (
     <div className="App">
-      <Header/>
+   
       <Router>
+      <Header/>
      {/*header*/}
           <Switch>
-            <Route path="/chat">
-              <h1>
-                Chatpage
-              </h1>
+            <Route path="/chats">
+             
             </Route>
             <Route path="/">
               <TinderCards/>
--- front_end/my-app/src/components/header/Header.js
+++ front_end/my-app/src/components/header/Header.js
@@ -3,6 +3,7 @@ import PersonIcon from '@material-ui/icons/Person';
 import QuestionAnswerIcon from '@material-ui/icons/QuestionAnswer';
 import "./Header.css";
 import IconButton from "@material-ui/core/IconButton";
+import {Link} from "react-router-dom";
 
 const Header = () => {
     return(
@@ -10,11 +11,16 @@ const Header = () => {
         <IconButton>
         <PersonIcon className="header__icon" fontSize="large"/>
         </IconButton>
-
+        <Link to="/">
         <img style={{height:'40px',objectFit:'contain'}} src="https://www.logosurfer.com/wp-content/uploads/2018/03/tinder-logo_0.png" alt="tinder logo"/>
+        </Link>
+   
+        <Link to="/chats">
         <IconButton>
         <QuestionAnswerIcon className="header__icon" fontSize="large"/>
         </IconButton>
+        </Link>
+        
         
     </div>
     )
